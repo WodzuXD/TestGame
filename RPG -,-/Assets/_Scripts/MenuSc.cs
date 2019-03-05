c2408ebb8f2e0a06446045618c5cb6add0cdab06
Wodzu <kruser757@gmail.com>
2019-03-05 09:21:36 +0100

    Add whole prodject

---
+++ RPG -,-/Assets/_Scripts/MenuSc.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSc : MonoBehaviour
+{
+    public int window = 0;
+
+    public GameObject[] okienko;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < okienko.Length; i++)
+        {
+            if (i == window)
+            {
+                okienko[i].active = true;
+            }
+            else
+                okienko[i].active = true;
+        }
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
