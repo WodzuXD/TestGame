192d94d5250766d63aa1748978546337c716d032
Woszu <kruser757@gmail.com>
2019-03-05 21:05:00 +0100

    Paragraph added

---
+++ _Git RPG/Assets/_Scripts/Gra.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gra : MonoBehaviour
+{
+    public int scena = 0;
+    public string name = "Derwan";
+    public GameObject[] panel;
+
+    public InputField playername;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < panel.Length; i++)
+        {
+            if (i == scena)
+                panel[i].active = true;
+            else
+                panel[i].active = false;
+        }
+    }
+
+    public void Wybor(int i)
+    {
+        scena = i;
+    }
+
+    public void SetName()
+    {
+        if (playername.text != "")
+            name = playername.text;
+        else
+            name = "Derwan";
+    }
+}
---
+++ _Git RPG/Assets/_Scripts/ScriptWithPlayerName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptWithPlayerName : MonoBehaviour
+{
+    public Text tekst;
+    public Gra game;
+
+    void Start()
+    {
+       
+    }
+
+
+    void Update()
+    {
+        tekst.text = game.name + ", interesujące... No dobra, Geruld, odprowadź " + game.name + "a do kwatery. Tracisz przytomność, budzisz się w kamiennym pokoju na wygodnym łożu";
+    }
+}
