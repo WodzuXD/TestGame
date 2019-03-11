c1e5294fd5e963f87e30be3426bcba17ee6d28d9
Woszu <kruser757@gmail.com>
2019-03-11 23:41:11 +0100

    add Music from "Jasna Strona Mocy"

---
+++ _Git RPG/Assets/_Scripts/JSMMusic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSMMusic : MonoBehaviour
+{
+
+    public AudioClip[] muzykaJSM;
+
+    int numer = 0;
+
+    public bool gra = false;
+
+    float timer = 0f;
+    float dlugoscPiosenki = 0f;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (!gra)
+        {
+            dlugoscPiosenki = muzykaJSM[numer].length;
+            timer = 0f;
+            MuzyczkaJSM();
+            gra = true;
+        }
+        if (timer >= dlugoscPiosenki)
+            numer = Random.Range(0, muzykaJSM.Length); gra = false;
+
+        timer += Time.deltaTime;
+    }
+
+    void MuzyczkaJSM()
+    {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(muzykaJSM[numer]);
+    }
+}
